f7eed8584f4fad3d9e5a34ca2295354bac0c885f
Millena Freitas <freitasmillena27@gmail.com>
2021-09-07 21:05:07 +0100

    Basic Static Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,21 @@
+import NavBar from "components/NavBar";
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá mundo!</h1> 
+      
+      <DataTable />
     </div>
+    
+    <Footer />
+    
+    </>
   );
 }
 
